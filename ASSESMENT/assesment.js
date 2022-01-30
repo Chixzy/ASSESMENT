f5302d96f5b990d0cbf6a369b465a1885ae885b6
Chixzy <chizitereofurum@gmail.com>
2022-01-30 10:34:43 +0100

    mysecond commit

---
+++ ASSESMENT/assesment.js
@@ -0,0 +1,10 @@
+const age = 10;
+let sol = age % 2;
+if (sol == 0) {
+    console.log("EVEN NUMBER");
+    
+} else {
+    console.log("ODD NUMBER");
+
+}
+
---
+++ firsty.js
@@ -0,0 +1,23 @@
+let myScore=70;
+//console.log(myScore);
+let chizy = 99
+const user = {
+    name: "chi",
+    sex: "male",
+    age: 22,
+    married: false,
+    siblings: ["toto", "yadiri", "chigirl"]
+    
+    
+};
+//console.log(user.married);
+//console.log(this);
+ function saymyname() {
+     //console.log("chizzy");
+    user.saymyname;
+    }
+    
+ function saysex() {
+    console.log(this.sex)
+}
+user. saysex();
